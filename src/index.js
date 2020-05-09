5b28ddcd4fa05051104c67d8ce29076dbd731fcb
Marcos Santos <marcosadriano034@gmail.com>
2020-05-09 17:38:01 -0300

    starting project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>Box Movie</div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
