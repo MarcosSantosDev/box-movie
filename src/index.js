52f8d04b98fd5f209aa410e21ba517aa012033f8
Marcos Santos <marcosadriano034@gmail.com>
2020-05-11 00:10:55 -0300

    fix: fixed code to pattern eslint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import 'normalize.css'
+import 'normalize.css';
 
-import Router from './components/Router'
+import Router from './components/Router';
 
 ReactDOM.render(
   <React.StrictMode>
